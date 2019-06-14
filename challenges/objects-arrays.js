91a15544e2f385644ffae4161bafa9b9f590514c
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-06-14 11:40:24 -0500

    SPrint challenge Javascript changes.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -64,7 +64,13 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
-console.log(contactInfo);
+//for (let i = 0; i < graduates.length; i++){
+ // contactInfo[i] = graduates[i]["first_name", "email" ];
+//}
+graduates.forEach(function(object){
+  let name = object.first_name+ " " +object.email;
+  contactInfo.push(name)})
+console.log(contactInfo)
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
